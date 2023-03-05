8809590a3dcc27d41bffb2392601af4bff8c07ff
Nolan <bugygamer.mc@gmail.com>
2023-03-04 17:51:57 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i=0; i <word.length /2; i++){
+    
+    const j = word.length -1 -i 
+    const stsrtChar = word[i]
+    const endchar = word[j]
+
+    if(stsrtChar !== endchar) return false
+
+  }
+  return true
+
 }
 
 /* 
   Add your pseudocode here
+
+  irrate from the beggin to the middle of the word
+    ceck the the letters
+      if any letters dont match return false 
 */
 
 /*
   Add written explanation of your solution here
+
+  make a fuction that retruns true with a palidrome
+  by reading the first and last letter of the paildrome 
 */
 
 // You can run `node index.js` to view these console logs
